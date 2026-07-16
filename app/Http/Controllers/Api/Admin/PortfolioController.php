8fe3fbbe642cad09cc641b52db8f31739c92fcbf
Arona Tech <2533035108@stu.istinye.edu.tr>
2026-07-17 01:14:09 +0300

    feat: add admin API controllers

---
+++ app/Http/Controllers/Api/Admin/PortfolioController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Portfolio;
+use Illuminate\Http\Request;
+
+class PortfolioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Portfolio $portfolio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Portfolio $portfolio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Portfolio $portfolio)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Api/Admin/ProjectImageController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProjectImage;
+use Illuminate\Http\Request;
+
+class ProjectImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProjectImage $projectImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProjectImage $projectImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProjectImage $projectImage)
+    {
+        //
+    }
+}
